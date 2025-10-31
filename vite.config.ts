651fdcd2733ecf37748f898347e30b80fac85bcf
Hesam Khosravi <113598960+hesamkhosravii@users.noreply.github.com>
2025-10-31 22:29:47 +0330

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       process.env.VITE_BOLT_DATABASE_URL || 'https://ujgjonnciemjmbbeygod.supabase.co'
     ),
     'import.meta.env.VITE_BOLT_DATABASE_ANON_KEY': JSON.stringify(
-      process.env.VITE_BOLT_DATABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3eWxsdmJkY3BwbGNicnFzdm5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDg5NzcsImV4cCI6MjA3NjcyNDk3N30.E775d9UFKrQmDphceFlkeFwbaK9Qib6YJWXhFUZCvCQ'
+      process.env.VITE_BOLT_DATABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqZ2pvbm5jaWVtam1iYmV5Z29kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDkwMTYsImV4cCI6MjA3NjcyNTAxNn0.SWqk0hje3cHmJlqHd1_CO62P7nqgOuOWxnJ0mBumldM'
     ),
   },
 });
