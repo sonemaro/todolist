b2a3a34364cc1b3dc57b062f30a3a620155cf99e
Hesam Khosravi <113598960+hesamkhosravii@users.noreply.github.com>
2025-10-31 22:28:57 +0330

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   define: {
     'import.meta.env.VITE_BOLT_DATABASE_URL': JSON.stringify(
-      process.env.VITE_BOLT_DATABASE_URL || 'https://qwyllvbdcpplcbrqsvnk.supabase.co'
+      process.env.VITE_BOLT_DATABASE_URL || 'https://ujgjonnciemjmbbeygod.supabase.co'
     ),
     'import.meta.env.VITE_BOLT_DATABASE_ANON_KEY': JSON.stringify(
       process.env.VITE_BOLT_DATABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3eWxsdmJkY3BwbGNicnFzdm5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDg5NzcsImV4cCI6MjA3NjcyNDk3N30.E775d9UFKrQmDphceFlkeFwbaK9Qib6YJWXhFUZCvCQ'
